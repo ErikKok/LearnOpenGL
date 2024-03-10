8fb2a9aa80bce993a68b3c4c10428480be274dbd
Erik <a@b.com>
2024-03-10 12:57:40 +0100

    Camera

--- LearnOpenGL/Main.cpp
+++ LearnOpenGL/Main.cpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "Camera.h"
 #include "Shader.h"
 // External header warning level: 0
 #include <glad/glad.h>
@@ -14,6 +15,18 @@
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = 1200 / 2.0f;
+float lastY = 900 / 2.0f;
+bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
 
 int main()
 {
@@ -33,71 +46,140 @@ int main()
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    // perspective correction
+    glm::mat4 projection{ glm::perspective(glm::radians(45.0f), 1200.0f / 900.0f, 0.1f, 100.0f) };
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cout << "Failed to initialize GLAD" << '\n';
         return -1;
     }
 
-    Shader ourShader("3.3.shader.vs", "3.3.shader.fs");
+    glEnable(GL_DEPTH_TEST);
 
     // Vertex data
 
-    //constexpr std::array vertices0{ // /\/\
-    // -0.5f,  0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
-    // -0.25f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
-    //  0.0f,  0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
-    //  0.25f, 0.5f, 0.0f, 0.0f, 1.0f, 1.0f,
-    //  0.5f,  0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
-    //};
+    constexpr std::array vertices0{
+            -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+             0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+             0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+             0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+             0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+            -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+             0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+             0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+             0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+             0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+             0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+             0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+             0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+             0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+             0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+             0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+    };
+
+    constexpr glm::vec3 cubePositions[] = {
+        glm::vec3(1.0f,  0.5f,  0.0f),
+        glm::vec3(2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3(2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3(1.3f, -2.0f, -2.5f),
+        glm::vec3(1.5f,  2.0f, -2.5f),
+        glm::vec3(1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
+
+    constexpr std::array xyz{ // Hex
+    // Positions            // Colors        
+     -10.0f,   0.0f,  0.0f,   1.0f, 0.0f, 0.0f,
+      10.0f,   0.0f,  0.0f,   1.0f, 0.0f, 0.0f,
+      0.0f,  -10.0f,  0.0f,   0.0f, 1.0f, 0.0f,
+      0.0f,   10.0f,  0.0f,   0.0f, 1.0f, 0.0f,   
+      0.0f,    0.0f, -10.0f,  0.0f, 0.0f, 1.0f,
+      0.0f,    0.0f,  10.0f,  0.0f, 0.0f, 1.0f,
+    };
 
     //constexpr std::array indices0{
     // 0u, 1u, 2u,  // first Triangle
     // 2u, 3u, 4u   // second Triangle
     //};
 
-    constexpr std::array vertices0{ // Hex
-    // Positions            // Colors          // Texture
-      0.0f,   0.0f,  0.0f,  0.0f, 0.0f, 0.0f,  0.5f, 0.5f,  // 0
-      0.0f,   1.0f,  0.0f,  1.0f, 0.0f, 0.0f,  0.5f, 1.0f,  // 1
-      0.67f,  0.50f, 0.0f,  0.0f, 1.0f, 0.0f,  1.0f, 0.75f, // 2
-      0.67f, -0.50f, 0.0f,  0.0f, 0.0f, 1.0f,  1.0f, 0.25f, // 3   
-      0.0f,  -1.0f,  0.0f,  0.0f, 1.0f, 1.0f,  0.5f, 0.0f,  // 4
-     -0.67f, -0.50f, 0.0f,  1.0f, 1.0f, 0.0f,  0.0f, 0.25f, // 5
-     -0.67f,  0.50f, 0.0f,  1.0f, 0.0f, 1.0f,  0.0f, 0.75f, // 6
-    };
+    //constexpr std::array vertices0{ // Hex
+    //// Positions            // Colors          // Texture
+    //  0.0f,   0.0f,  0.0f,  0.0f, 0.0f, 0.0f,  0.5f, 0.5f,  // 0
+    //  0.0f,   1.0f,  0.0f,  1.0f, 0.0f, 0.0f,  0.5f, 1.0f,  // 1
+    //  0.67f,  0.50f, 0.0f,  0.0f, 1.0f, 0.0f,  1.0f, 0.75f, // 2
+    //  0.67f, -0.50f, 0.0f,  0.0f, 0.0f, 1.0f,  1.0f, 0.25f, // 3   
+    //  0.0f,  -1.0f,  0.0f,  0.0f, 1.0f, 1.0f,  0.5f, 0.0f,  // 4
+    // -0.67f, -0.50f, 0.0f,  1.0f, 1.0f, 0.0f,  0.0f, 0.25f, // 5
+    // -0.67f,  0.50f, 0.0f,  1.0f, 0.0f, 1.0f,  0.0f, 0.75f, // 6
+    //};
 
-    constexpr std::array indices0{
-     0u, 1u, 2u,
-     0u, 2u, 3u,
-     0u, 3u, 4u,
-     0u, 4u, 5u,
-     0u, 5u, 6u,
-     0u, 6u, 1u,
-    };
+    //constexpr std::array indices0{
+    // 0u, 1u, 2u,
+    // 0u, 2u, 3u,
+    // 0u, 3u, 4u,
+    // 0u, 4u, 5u,
+    // 0u, 5u, 6u,
+    // 0u, 6u, 1u,
+    //};
+
+    // Cube
+
+    unsigned int vao0; // vertex array object
+    glGenVertexArrays(1, &vao0);
+    glBindVertexArray(vao0);
 
     unsigned int vbo0; // vertex buffer object
     glGenBuffers(1, &vbo0);
     glBindBuffer(GL_ARRAY_BUFFER, vbo0);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices0), &vertices0, GL_STATIC_DRAW);
 
-    unsigned int vao0; // vertex array object
-    glGenVertexArrays(1, &vao0);
-    glBindVertexArray(vao0);
+    //unsigned int ebo0; // element buffer object
+    //glGenBuffers(1, &ebo0);
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo0);
+    //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices0), &indices0, GL_STATIC_DRAW);
 
-    unsigned int ebo0; // element buffer object
-    glGenBuffers(1, &ebo0);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo0);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices0), &indices0, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-    glEnableVertexAttribArray(2);
+    //glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(6 * sizeof(float)));
+    //glEnableVertexAttribArray(2);
 
-    //glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // Cube Texture
 
     unsigned char* textureData{};
     int textureWidth{};
@@ -106,14 +188,11 @@ int main()
 
     unsigned int texture0;
     glGenTextures(1, &texture0);
-    glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, texture0);
-    // set the texture wrapping/filtering options (on the currently bound texture object)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    // load and generate the texture
     stbi_set_flip_vertically_on_load(true);
     textureData = stbi_load("container.jpg", &textureWidth, &textureHeight, &textureNrChannels, 0);
     if (textureData) {
@@ -123,18 +202,15 @@ int main()
     else {
         std::cout << "Failed to load texture" << '\n';
     }    
-    //stbi_image_free(textureData);
+    stbi_image_free(textureData);
 
     unsigned int texture1;
     glGenTextures(1, &texture1);
-    glActiveTexture(GL_TEXTURE1);
     glBindTexture(GL_TEXTURE_2D, texture1);
-    // set the texture wrapping/filtering options (on the currently bound texture object)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    // load and generate the texture
     stbi_set_flip_vertically_on_load(true);
     textureData = stbi_load("awesomeface.png", &textureWidth, &textureHeight, &textureNrChannels, 0);
     if (textureData) {
@@ -146,61 +222,84 @@ int main()
     }
     stbi_image_free(textureData);
 
+    // Cube Shader
+
+    Shader ourShader("3.3.shader.vs", "3.3.shader.fs");
     ourShader.use();
-    glUniform1i(glGetUniformLocation(ourShader.id, "texture0"), 0);
-    glUniform1i(glGetUniformLocation(ourShader.id, "texture1"), 1);
+    ourShader.setMat4("projection", projection); // TODO in render loop for zooming 
+    ourShader.setInt("texture0", 0);
+    ourShader.setInt("texture1", 1);
 
+    // XYZ
 
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    unsigned int vao1; // vertex array object
+    glGenVertexArrays(1, &vao1);
+    glBindVertexArray(vao1);
+
+    unsigned int vbo1; // vertex buffer object
+    glGenBuffers(1, &vbo1);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo1);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(xyz), &xyz, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    Shader xyzShader("xyzShader.vs", "xyzShader.fs");
+    xyzShader.use();
+    xyzShader.setMat4("projection", projection);
 
     while (!glfwWindowShouldClose(window))
     {
+        // per-frame time logic
+        float currentFrame = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+        
         processInput(window);
 
-        // Matrix
-        glm::mat4 trans{ glm::mat4(1.0f) };
-        trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
-        trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0, 0.0, 1.0));
-        trans = glm::scale(trans, glm::vec3(0.5, -0.5, 0.0));
-
-        glUniformMatrix4fv(glGetUniformLocation(ourShader.id, "transform"), 1, GL_FALSE, glm::value_ptr(trans));
-
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glm::mat4 view = camera.GetViewMatrix();
+
+        // XYZ
+
+        xyzShader.use();
+        xyzShader.setMat4("view", view);
+        glBindVertexArray(vao1);
+        glDrawArrays(GL_LINES, 0, static_cast<GLsizei>(xyz.size()));
+        glBindVertexArray(0);
+
+        // Cube
    
-        glBindVertexArray(vao0);
+        ourShader.use();
+        ourShader.setMat4("view", view);
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture0);
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture1);
 
-        glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(vertices0.size()), GL_UNSIGNED_INT, 0);
+        glBindVertexArray(vao0);
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            glm::mat4 model{ glm::mat4(1.0f) };
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            //model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            model = glm::rotate(model, (float)glfwGetTime() * glm::radians(100.0f) * glm::radians(angle), glm::vec3(0.5f, 1.0f, 0.0f));
+            ourShader.setMat4("model", model);
 
-        // Matrix
-        //glm::mat4 trans{ glm::mat4(1.0f) };
-        trans = glm::translate(trans, glm::vec3(-0.5f, 0.5f, 0.0f));
-        trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0, 0.0, 1.0));
-        trans = glm::scale(trans, glm::vec3(0.5, -0.5, 0.0));
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+        glBindVertexArray(0);
 
-        glUniformMatrix4fv(glGetUniformLocation(ourShader.id, "transform"), 1, GL_FALSE, glm::value_ptr(trans));
 
-        glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(vertices0.size()), GL_UNSIGNED_INT, 0);
-
-        trans = glm::mat4(1.0f);
-        trans = glm::translate(trans, glm::vec3(-0.5f, 0.5f, 0.0f));
-        trans = glm::scale(trans, glm::vec3(static_cast<float>(sin(glfwGetTime())), static_cast<float>(sin(glfwGetTime())), static_cast<float>(sin(glfwGetTime()))));
-
-        glUniformMatrix4fv(glGetUniformLocation(ourShader.id, "transform"), 1, GL_FALSE, &trans[0][0]);
-
-        glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(vertices0.size()), GL_UNSIGNED_INT, 0);
-
-        //float mixAmount{ 0.0 };
-        //for (int i = 0; i < 100; i++)
-        //{
-        //    ourShader.setFloat("mixAmount", mixAmount);
-        //    mixAmount += 0.01f;
-        //    glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(vertices0.size()), GL_UNSIGNED_INT, 0);
-        //}
+        //glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(vertices0.size()), GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -208,7 +307,9 @@ int main()
 
     glDeleteVertexArrays(1, &vao0);
     glDeleteBuffers(1, &vbo0);
-    glDeleteBuffers(1, &ebo0);
+    glDeleteVertexArrays(1, &vao1);
+    glDeleteBuffers(1, &vbo1);
+    //glDeleteBuffers(1, &ebo0);
 
     glfwTerminate();
     return 0;
@@ -217,11 +318,50 @@ int main()
 void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);       
+        glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime);
 }
 
 #pragma warning( suppress : 4100 ) //#pragma warning( default : 4100 )
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
+}
+
+#pragma warning( suppress : 4100 )
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);
+
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+#pragma warning( suppress : 4100 )
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    camera.ProcessMouseScroll(static_cast<float>(yoffset));
 }
